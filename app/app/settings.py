13711e6e1f410f4410f9a7e80725abd97913273a
Benjamin <kodiugos@gmail.com>
2023-09-18 10:08:39 -1100

    included restframework authtoken in settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,10 +40,11 @@ INSTALLED_APPS = [
     # Installed app
     'core',
     'user',
-    'rest_framework',
 
     # Third_party apps
     'drf_spectacular',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
