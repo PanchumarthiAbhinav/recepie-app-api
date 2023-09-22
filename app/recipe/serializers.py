35628f0ab632d2b0a2f6f38a80bbde628e7852dd
Benjamin <kodiugos@gmail.com>
2023-09-21 22:25:25 -1100

    Implemented recipe detail API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -9,5 +9,12 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'description', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,13 +5,13 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from .serializers import RecipeSerializer
+from .serializers import RecipeSerializer, RecipeDetailSerializer
 
 
 # Create your views here.
 class RecipeViewSets(viewsets.ModelViewSet):
     """view for manage recipe APIs."""
-    serializer_class = RecipeSerializer
+    serializer_class = RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -19,3 +19,8 @@ class RecipeViewSets(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for authenticated users only"""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return RecipeSerializer
+        return self.serializer_class
